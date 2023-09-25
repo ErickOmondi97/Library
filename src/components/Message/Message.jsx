05e0c1658a11b75af3962e4394341ed4980dd39b
Erick Omondi <eriqueasaria@gmail.com>
2023-09-25 22:16:13 +0300

    I added the folder which includes the component and partial scss file for the message  section

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Message() {
+  return (
+    <div className='messageSection'>
+      <h1 className="Message">
+        Welcome Message
+      </h1>
+      <p className="MessageParagraph">
+        Lorem ipsum iweieuwiriu lorm ajdjhoiioshfbjbjhbh bjhgudhjhfbufguy djfhgusjfbjfjk <br />
+        gyuguruih ruiyuisfuih jfhugyusgufhbjfkjj hgyufgyufuifhijfh jfhuifhuifhufhuifh <br />
+        byuhrgyurhhrh hgryurgyurghrbh ryhggur hrgyurgrhrj hrgyurgru rhruhrrui hrururu <br />
+        hghagsytydfgyfio dhgfyufgyfui ffufuuffhjf fhfyfufhfhjjfhfj hufyhfiufufufhfh hfufuufufu 
+      </p>
+      <a href="#" className="btn">LEARN MORE</a>
+    </div>
+  )
+}
+
+export default Message
