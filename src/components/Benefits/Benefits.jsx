ed8aa0d2c05d6e29810160b84a41770fbd9ee872
Erick Omondi <eriqueasaria@gmail.com>
2023-09-25 22:15:32 +0300

    I added the folder which includes the component and partial scss file for the benefits section

---
+++ src/components/Benefits/Benefits.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Benefits() {
+  return (
+    <div className='benefitsSection'>
+      <h1 className='benefitsTitle'>
+        Benefits of working with us
+      </h1>
+
+      <div className="benefitsCards">
+        <div className="benefitscard">
+          <h2>01</h2>
+          <h3>High Quality</h3>
+          <p>Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft <br /> avdtdft agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt <br /> adtdfdt ff da daaas addf addd ade addfe afgtgr Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft avdtdft <br /> agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt adtdfdt ff da daaas addf addd ade addfe afgtgr</p>
+        </div>
+
+        <div className="benefitscard">
+          <h2>02</h2>
+          <h3>High Quality</h3>
+          <p>Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft <br /> avdtdft agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt <br /> adtdfdt ff da daaas addf addd ade addfe afgtgr Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft avdtdft <br /> agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt adtdfdt ff da daaas addf addd ade addfe afgtgr</p>
+        </div>
+
+        <div className="benefitscard">
+          <h2>03</h2>
+          <h3>High Quality</h3>
+          <p>Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft <br /> avdtdft agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt <br /> adtdfdt ff da daaas addf addd ade addfe afgtgr Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft avdtdft <br /> agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt adtdfdt ff da daaas addf addd ade addfe afgtgr</p>
+        </div>
+
+        <div className="benefitscard1">
+          <h2>04</h2>
+          <h3>High Quality</h3>
+          <p>Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft <br /> avdtdft agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt <br /> adtdfdt ff da daaas addf addd ade addfe afgtgr Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft avdtdft <br /> agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt adtdfdt ff da daaas addf addd ade addfe afgtgr</p>
+        </div>
+
+        <div className="benefitscard">
+          <h2>05</h2>
+          <h3>High Quality</h3>
+          <p>Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft <br /> avdtdft agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt <br /> adtdfdt ff da daaas addf addd ade addfe afgtgr Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft avdtdft <br /> agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt adtdfdt ff da daaas addf addd ade addfe afgtgr</p>
+        </div>
+
+        <div className="benefitscard1">
+          <h2>06</h2>
+          <h3>High Quality</h3>
+          <p>Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft <br /> avdtdft agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt <br /> adtdfdt ff da daaas addf addd ade addfe afgtgr Lorem ipsum hatyugdy agy asgydg atdfdt advtd afdftdf avdgdft avdtdft <br /> agdtdft avdtdftd adgtdft agdvtdgft adtdftd adtdfdt adtdfdt adtdfdt adtdfdt ff da daaas addf addd ade addfe afgtgr</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Benefits
