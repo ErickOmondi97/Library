09f81dce6e2da0cc272d952fe8758f84947f840f
Erick Omondi <eriqueasaria@gmail.com>
2023-09-26 22:42:53 +0300

    I added the coaching section, including the jsx filena and scss partial file

---
+++ src/components/Coaching/Coaching.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Coaching() {
+  return (
+    <div className='coachingSection'>
+      <h1 className="coachingTitle">
+        How Coaching Works
+      </h1>
+      <p>
+        Simple Text. Lorem agdgd agdgeehh age ahhahha ehhhe ahhe ahhhhhe hahhefbbd ahdgdggve avdgdgve avggvd <br />
+        gdgfdtaftdyfg dhdgyavdvy dgatydf advtdfyd adgydgfd dgtydf.
+      </p>
+      <div className="buttons">
+        <a href="#" className="btn">LEARN MORE</a>
+        <a href="#" className="btn">LIVE DEMO</a>
+      </div>
+    </div>
+  )
+}
+
+export default Coaching
