044e4eb008b2608c2532049fbb7a7caa4fc5ae46
Erick Omondi <eriqueasaria@gmail.com>
2023-09-28 21:50:51 +0300

    I added more code to style the Where section

--- src/components/Where/Where.jsx
+++ src/components/Where/Where.jsx
@@ -3,7 +3,52 @@ import React from 'react'
 function Where() {
   return (
     <div className='whereSection'>
-      
+      <div className="whereHeading">
+        <h1 className="whereTitle">
+          How and where to <br /> learn mindfulness
+        </h1>
+        <p>
+        Lorem ipsum iweieuwiriu lorm ajdjhoiioshfbjbjhbh bjhgudhjhfbufguy djfhgusjfbjfjk <br />
+        gyuguruih ruiyuisfuih jfhugyusgufhbjfkjj hgyufgyufuifhijfh jfhuifhuifhufhuifh <br />
+        byuhrgyurhhrh hgryurgyurghrbh ryhggur hrgyurgrhrj hrgyurgru rhruhrrui hrururu <br />
+        hghagsytydfgyfio dhgfyufgyfui ffufuuffhjf fhfyfufhfhjjfhfj hufyhfiufufufhfh hfufuufufu
+        </p>
+      </div>
+
+      <div className="whereBottom">
+        <div className="exp">
+          <h3>
+            Introductory courses, taster <br /> sessions and groups <br /> Formal mindfulness courses
+          </h3>
+
+          <p>
+          Sample Text. Click to Select Text Element
+          </p>
+        </div>
+
+        <div className="figures">
+          <div className="figure">
+            <h1>5k</h1>
+            <p>Sample Headline</p>
+          </div>
+          
+          <div className="figure">
+            <h1>60%</h1>
+            <p>Sample Headline</p>
+          </div>
+          
+          <div className="figure">
+            <h1>100</h1>
+            <p>Sample Headline</p>
+          </div>
+          
+          <div className="figure">
+            <h1>1230</h1>
+            <p>Sample Headline</p>
+          </div>
+          
+        </div>
+      </div>
     </div>
   )
 }
