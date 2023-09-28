d6de191ce629e5defebb02c11f15523f54710fa4
Erick Omondi <eriqueasaria@gmail.com>
2023-09-28 22:03:30 +0300

    I added the meet component

---
+++ src/components/Meet/Meet.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function Meet() {
+  return (
+    <div className='meetSection'>
+      <div className="title">
+        <h1>Meet The Team <br /> <span>Our Professionals</span></h1>
+      </div>
+
+      <div className="professionals">
+        <div className="professional">
+          <img src="" alt="professional" />
+          <h4>mary Brown</h4>
+          <h5>creative leader</h5>
+          <p>Simple Text. Lorem agdgd agdgeehh age ahhahha ehhhe ahhe ahhhhhe hahhefbbd ahdgdggve avdgdgve avggvd <br />
+            gdgfdtaftdyfg dhdgyavdvy dgatydf advtdfyd adgydgfd dgtydf.</p>
+
+            <div className="socials">
+              <img src="" alt="facebook" />
+              <img src="" alt="instagram" />
+              <img src="" alt="linkedin" />
+              <img src="" alt="twitter" />
+            </div>
+        </div>
+
+        <div className="professional">
+          <img src="" alt="professional" />
+          <h4>Ann Ricjmond</h4>
+          <h5>creative leader</h5>
+          <p>Simple Text. Lorem agdgd agdgeehh age ahhahha ehhhe ahhe ahhhhhe hahhefbbd ahdgdggve avdgdgve avggvd <br />
+            gdgfdtaftdyfg dhdgyavdvy dgatydf advtdfyd adgydgfd dgtydf.</p>
+
+            <div className="socials">
+              <img src="" alt="facebook" />
+              <img src="" alt="instagram" />
+              <img src="" alt="linkedin" />
+              <img src="" alt="twitter" />
+            </div>
+        </div>
+        
+        <div className="professional">
+          <img src="" alt="professional" />
+          <h4>Bob Greenfield</h4>
+          <h5>creative leader</h5>
+          <p>Simple Text. Lorem agdgd agdgeehh age ahhahha ehhhe ahhe ahhhhhe hahhefbbd ahdgdggve avdgdgve avggvd <br />
+            gdgfdtaftdyfg dhdgyavdvy dgatydf advtdfyd adgydgfd dgtydf.</p>
+
+            <div className="socials">
+              <img src="" alt="facebook" />
+              <img src="" alt="instagram" />
+              <img src="" alt="linkedin" />
+              <img src="" alt="twitter" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Meet
