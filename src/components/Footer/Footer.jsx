64df63e856d36cba09b50e1c67534a6258fb3681
Erick Omondi <eriqueasaria@gmail.com>
2023-09-28 21:58:32 +0300

    I added this footer component for the UI

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <div className="left">
+        <h2>Start using our App for free</h2>
+        <p>Simple Text. Lorem agdgd agdgeehh age ahhahha ehhhe ahhe ahhhhhe hahhefbbd ahdgdggve avdgdgve avggvd <br />
+            gdgfdtaftdyfg dhdgyavdvy dgatydf advtdfyd adgydgfd dgtydf.</p>
+        <div className="search"><input type="text" placeholder='Enter a valid email address'/> <a href="#">Submit</a></div>
+        <p>Or compare plans from Ksh.1700 per month</p>
+      </div>
+
+      <div className="right">
+        <p>safe and secure</p>
+        <p>cancel anytime</p>
+        <p>24/7 Support</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
